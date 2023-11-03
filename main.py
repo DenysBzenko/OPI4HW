730569d9a81438227625541be8333b981da7668e
Denys <119534908+DenysBzenko@users.noreply.github.com>
2023-11-03 17:05:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from flask import Flask, jsonify  # Виправлено імпорт
+from flask import Flask, jsonify  
 import os
 import logging
-from logging.handlers import RotatingFileHandler  # Додано імпорт
+from logging.handlers import RotatingFileHandler 
 from feature_one import user_total_time
 from feature_two import user_average_time
 from feature_three import forget_user
 
 app = Flask(__name__)
 
-# Визначення маршрутів безпосередньо
+
 app.add_url_rule('/api/stats/user/total', view_func=user_total_time, methods=['GET'])
 app.add_url_rule('/api/stats/user/average', view_func=user_average_time, methods=['GET'])
 app.add_url_rule('/api/user/forget', view_func=forget_user, methods=['POST'])
 
-# Error handlers
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({'error': 'Not found'}), 404
@@ -22,13 +22,13 @@ def not_found(error):
 def internal_error(error):
     return jsonify({'error': 'Internal server error'}), 500
 
-# Logging setup
 handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
 if __name__ == '__main__':
-    # Run the app with environment-specific settings
+
+    
     env = os.environ.get('APP_ENV', 'development')
     if env == 'production':
         app.run(debug=False)
