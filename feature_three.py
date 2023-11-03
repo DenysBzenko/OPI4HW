9230ba9f4ed58390f782993a393f2745e80e1ef4
Denys <119534908+DenysBzenko@users.noreply.github.com>
2023-11-03 17:05:36 +0200

    Update feature_three.py

--- feature_three.py
+++ feature_three.py
@@ -12,23 +12,23 @@ def delete_user_data_service(user_id):
 def forget_user():
     user_id = request.args.get('userId')
 
-    # Input validation
+   
     if user_id is None:
         return jsonify({"error": "userId is required"}), 400
 
     try:
         deletion_successful = delete_user_data_service(user_id)
     except Exception as e:
-        # Proper error handling, ensuring no sensitive user data is logged
+        
         return jsonify({"error": "An error occurred while processing the request"}), 500
 
     if deletion_successful:
-        # Respond with the user ID to confirm the deletion has been processed
+        
         return jsonify({"userId": user_id})
     else:
-        # Handle the deletion error properly
+       
         return jsonify({"error": "An error occurred while processing the request"}), 500
 
 if __name__ == '__main__':
-    # Use environment variable for configuration
+  
     app.run(debug=os.getenv('FLASK_DEBUG', 'False').lower() == 'true')
